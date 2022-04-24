a2a9e2c92a2c6548cc2e830f7ca0c6e67679c78e
Anthony Androulakis <aandroulakis@zoho.com>
2022-04-24 12:45:56 -0400

    update deps + GA
    dependencies + google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -10,15 +10,11 @@ function MyApp({ Component, pageProps }) {
     <div>
       <Head>
 
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9W9WPPYMEF"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-         
-          gtag('config', 'G-9W9WPPYMEF');
-        </script>
+        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-9W9WPPYMEF');`}} />
 
 
         <link type="text/css" rel="stylesheet" href="/styles.css" as="style"></link>
@@ -27,6 +23,8 @@ function MyApp({ Component, pageProps }) {
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.png" />
 
       </Head>
+
+      <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-9W9WPPYMEF" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
 
       <Header />
       <Component {...pageProps} />
