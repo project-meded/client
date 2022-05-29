e1aa5853059c4df5d1855b7da19ca77005aa082c
Anthony Androulakis <aandroulakis@zoho.com>
2022-05-29 16:50:37 -0400

    updated ga
    https://github.com/vercel/next.js/tree/canary/examples/with-google-analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,56 @@
 import "antd/dist/antd.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
-import Head from "next/head";
 import "./font.css";
 import "../public/styles.css";
 
+import * as gtag from '../lib/gtag';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react'
+
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
-    <div>
-      <Head>
+    <>
 
-        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-9W9WPPYMEF');`}} />
-
-
-        <link type="text/css" rel="stylesheet" href="/styles.css" as="style"></link>
-
-        <title>Project MED</title>
-        <link rel="shortcut icon" type="image/x-icon" href="/favicon.png" />
-
-      </Head>
-
-      <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-9W9WPPYMEF" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
 
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </div>
+    </>
   );
 }
 
